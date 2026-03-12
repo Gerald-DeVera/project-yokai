41aa96279d6ecc38cf3b0c8006eaa451000a81cd
naeroo <daveednh@gmail.com>
2026-03-11 18:39:34 -0700

    dialogue can check for item in inv
    see global.gd

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -2,12 +2,24 @@ extends Node
 
 var player_pos: Vector2
 var camera_pos: Vector2
+var inv: Inv = preload("res://Assets/InventoryItems/playerInventory.tres")
+var playerHasItem: bool
 #foundQuests (should) consist of the string key/value pairs where the keys are quest names and
 #values are their descriptions
 var foundQuests: Dictionary
 #foundQuotes (should) do the same, but with keys being NPC names and values being the quotes
 var foundQuotes: Dictionary
 #This should help with better sorting of the Notebook in theory
+
+#Used in dialogue manager to look for a certain item, or can call for other events
+func searchInv(target_item: String):
+	for item in inv.items:
+		if item != null && item.name == target_item:
+			print(item.name)
+			playerHasItem = true
+			break
+		else:
+			playerHasItem = false
 
 func grabCameraPos():
 	camera_pos = get_viewport().get_camera_2d().position
