7c3223a0eaa5ec59c443415a32ce370265ea2553
Gerald De Vera <devergerald@gmail.com>
2026-03-28 11:35:48 -0700

    boulder
    boulder

---
+++ Scripts/entityScripts/boss.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@onready var path: Path2D = $"../BossChargePositions/Path2D"
+@onready var pathFollow: PathFollow2D = $"../BossChargePositions/Path2D/PathFollow2D"
+var moveSpeed: float = 1000
+var loopPath: bool = true
+
+var lastPosition: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if pathFollow:
+		print("not null")
+	else:
+		print("null")
+	#position = pathFollow.global_position
+	#lastPosition = position
+	pass # Replace with function body.
+
+func _physics_process(delta: float) -> void:
+	pathFollow.progress += moveSpeed * delta
+	position = pathFollow.position
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
